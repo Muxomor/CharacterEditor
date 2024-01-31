6ca58a8a3890ad6645da28450a29830baf06d732
muxomor <7baltika9@gmail.com>
2024-01-31 18:25:56 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/CRUD.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Driver;
+using System;
+
+namespace WpfApp1
+{
+    public class CRUD
+    {
+        public static void CreateCharacter(Character character)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Shamaev322");
+            var collection = database.GetCollection<Character>("Characters");
+            collection.InsertOne(character);
+        }
+
+        public static void GetCharacter(string name)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Shamaev322");
+            var collection = database.GetCollection<Character>("Characters");
+            var user = collection.Find(x => x.Name == name).FirstOrDefault();
+
+            if (user != null)
+                Console.WriteLine($"{user.Name} {user.ClassName}");
+            else
+                Console.WriteLine("Not found");
+        }
+
+    }
+}
---
+++ WpfApp1/Character.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+
+namespace WpfApp1
+{
+    public class Character
+    {
+        public ObjectId _id;
+        public string Name { get; set; }
+        public string ClassName { get; set; }
+        public int Strength { get; set; }
+        public int Intelligence { get; set; }
+        public int Vitality { get; set; }
+    }
+}
